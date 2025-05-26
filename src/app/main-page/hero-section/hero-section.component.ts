4eff7cb866d527468c215994e8322abfc6d9e748
Marco <marco.palummieri@gmail.com>
2025-05-26 20:58:56 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/main-page/hero-section/hero-section.component.ts
+++ src/app/main-page/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
+import { MyStackSectionComponent } from './my-stack-section/my-stack-section.component';
+import { MyCraftSectionComponent } from './my-craft-section/my-craft-section.component';
+import { TestimonialsSectionComponent } from './testimonials-section/testimonials-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
+import { NavbarComponent } from './hero-section/navbar/navbar.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeroSectionComponent], //Hier richtig eingesetzt!??
+  imports: [NavbarComponent, HeroSectionComponent, AboutMeSectionComponent, MyStackSectionComponent, MyCraftSectionComponent, TestimonialsSectionComponent, ContactSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
