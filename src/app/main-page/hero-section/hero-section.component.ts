be4cce521c0a6a46499be5f1199fd0d70c91cd08
Marco <marco.palummieri@gmail.com>
2025-05-27 18:38:05 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/main-page/hero-section/hero-section.component.ts
+++ src/app/main-page/hero-section/hero-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
@@ -8,6 +8,32 @@ import { NavbarComponent } from './navbar/navbar.component';
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
-export class HeroSectionComponent {
+// export class HeroSectionComponent {
 
+// }
+
+export class HeroSectionComponent implements AfterViewInit {
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.initializeBouncingLetters();
+  }
+
+  private initializeBouncingLetters(): void {
+    const bouncingLetters = this.elementRef.nativeElement.querySelectorAll('.bouncing-letters > span');
+    
+    bouncingLetters.forEach((element: HTMLElement) => {
+      element.addEventListener('mouseover', (e) => this.bounce(e.target as HTMLElement));
+    });
+  }
+
+  private bounce(letter: HTMLElement): void {
+    if (!letter.classList.contains('bounce')) {
+      letter.classList.add('bounce');
+      setTimeout(() => {
+        letter.classList.remove('bounce');
+      }, 1000);
+    }
+  }
 }
