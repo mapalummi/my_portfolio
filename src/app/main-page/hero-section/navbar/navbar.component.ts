a10ec56d5f29ee89430204063fed447eb5bc4cba
Marco <marco.palummieri@gmail.com>
2025-05-29 16:47:26 +0200

    Refactor language toggle implementation by adding a new toggle switch design and removing unused icons

--- src/app/main-page/hero-section/navbar/navbar.component.ts
+++ src/app/main-page/hero-section/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { AboutMeSectionComponent } from "../../about-me-section/about-me-section.component";
+import { AboutMeSectionComponent } from '../../about-me-section/about-me-section.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [AboutMeSectionComponent], //CHECK: Warum hab ich die hier rein gestezt??
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isEnglish = false; // Toggle-Status
 
+  toggleLanguage() {
+    this.isEnglish = !this.isEnglish;
+  }
 }
-
-//TODO: if/else für toggle language !!!
