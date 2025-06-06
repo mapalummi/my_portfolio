cd895a11a63c5a544e10c4850ccb53916f1d7752
Marco <marco.palummieri@gmail.com>
2025-06-06 12:29:07 +0200

    Refactor component structure: move Join, El Pollo Loco, and Da Bubble components to a new pages directory; update imports and routes accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from './header/header.component';
-import { JoinComponent } from './main-page/join/join.component';
-import { ElPolloLocoComponent } from './main-page/el-pollo-loco/el-pollo-loco.component';
-import { DaBubbleComponent } from './main-page/da-bubble/da-bubble.component';
+import { JoinComponent } from './pages/join/join.component';
+import { ElPolloLocoComponent } from './pages/el-pollo-loco/el-pollo-loco.component';
+import { DaBubbleComponent } from './pages/da-bubble/da-bubble.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { JoinComponent } from './main-page/join/join.component';
-import { ElPolloLocoComponent } from './main-page/el-pollo-loco/el-pollo-loco.component';
-import { DaBubbleComponent } from './main-page/da-bubble/da-bubble.component';
+import { JoinComponent } from './pages/join/join.component';
+import { ElPolloLocoComponent } from './pages/el-pollo-loco/el-pollo-loco.component';
+import { DaBubbleComponent } from './pages/da-bubble/da-bubble.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
