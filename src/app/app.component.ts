d6c45436cfebe243accb27594a7db5ecfe5c06f1
Marco <marco.palummieri@gmail.com>
2025-06-06 15:44:47 +0200

    Refactor imports and structure: comment out unused component imports in app.component.ts; update join.component.ts to include HeaderComponent; adjust join.component.html for improved formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from './header/header.component';
-import { JoinComponent } from './pages/join/join.component';
-import { ElPolloLocoComponent } from './pages/el-pollo-loco/el-pollo-loco.component';
-import { DaBubbleComponent } from './pages/da-bubble/da-bubble.component';
+// import { FooterComponent } from "./footer/footer.component";
+// import { HeaderComponent } from './header/header.component';
+// import { JoinComponent } from './pages/join/join.component';
+// import { ElPolloLocoComponent } from './pages/el-pollo-loco/el-pollo-loco.component';
+// import { DaBubbleComponent } from './pages/da-bubble/da-bubble.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JoinComponent, ElPolloLocoComponent, DaBubbleComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/join/join.component.ts
+++ src/app/pages/join/join.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss'
 })
