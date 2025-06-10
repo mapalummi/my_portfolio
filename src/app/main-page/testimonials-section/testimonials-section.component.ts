4c46bf13b0ba63d68ecc89b626c1cb51b2b370c1
Marco <marco.palummieri@gmail.com>
2025-06-10 22:20:46 +0200

    Enhance testimonials section for internationalization support by updating translation files and refactoring HTML structure

--- src/app/main-page/testimonials-section/testimonials-section.component.ts
+++ src/app/main-page/testimonials-section/testimonials-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-testimonials-section',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './testimonials-section.component.html',
   styleUrl: './testimonials-section.component.scss'
 })
