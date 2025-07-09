152666fa70735c35938b8137699e1be812f0840b
Marco <marco.palummieri@gmail.com>
2025-07-09 16:40:30 +0200

    Implement OnInit lifecycle hook in PrivacyPolicyComponent to scroll to top on initialization

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
@@ -11,8 +11,12 @@ import { TranslatePipe } from '@ngx-translate/core';
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit {
   constructor(private router: Router) { }
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+  }
 
   goBack(): void {
     this.router.navigate(['/']).then(() => {
