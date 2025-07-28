bcaa6feb0afdc5ad43ef89e5ade10515de5c64f0
Marco <marco.palummieri@gmail.com>
2025-07-28 17:53:20 +0200

    refactor: enhance contact success dialog with translation support and improved messaging

--- src/app/contact-success-dialog/contact-success-dialog.component.ts
+++ src/app/contact-success-dialog/contact-success-dialog.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogRef } from '@angular/material/dialog';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-success-dialog',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, TranslatePipe],
   templateUrl: './contact-success-dialog.component.html',
   styleUrl: './contact-success-dialog.component.scss',
 })
